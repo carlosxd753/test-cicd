1a471b687f68c2df8f36a75fb99b7a62c4b0eaab
Carlos Basurto <carlosxd753@gmail.com>
2024-11-28 13:14:18 -0500

    Done

---
+++ src/main/java/com/example/demo1/GreetingController.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greetings")
+    public Mono<String> helloWorld(){return Mono.just("Hello World");}
+}
